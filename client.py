47211ad7c29ee346eafc9a72e63eb0db57e6368e
Alexey Zakhlestin <indeyets@gmail.com>
2011-03-02 19:28:50 +0300

    we do not need username

--- client.py
+++ client.py
@@ -1,19 +1,21 @@
 import zmq
 
-def main(addr, who):
+def main(addr):
     ctx = zmq.Context()
     socket = ctx.socket(zmq.REQ)
     socket.connect(addr)
 
     while True:
-        msg = bytes(input("%s> " % who), 'utf8')
+        msg = bytes(input("enter message> "), 'utf8')
         socket.send(msg)
         resp = socket.recv()
         print("got: %s\n" % (resp))
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 3:
-        print("usage: client.py <address> <username>")
+
+    if len(sys.argv) != 2:
+        print("usage: client.py <address>")
         raise SystemExit
-    main(sys.argv[1], sys.argv[2])
+
+    main(sys.argv[1])
