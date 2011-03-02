346ae5b0ab09a15e61e22310a01b785bbcb06c0a
Alexey Zakhlestin <indeyets@gmail.com>
2011-03-02 19:08:30 +0300

    dummy daemon implementation

---
+++ daemon.py
@@ -0,0 +1,41 @@
+import json_ld_processor as jlp
+import zmq
+import pygtk
+import gi
+
+from gi.repository import Midgard
+
+def init():
+    Midgard.init()
+
+    config = Midgard.Config()
+    config.props.dbtype = "SQLite"
+    config.set_property ("database", "testdb")
+    config.set_property ("loglevel", "warn")
+
+    mgd = Midgard.Connection()
+    mgd.open_config (config)
+
+    storage = Midgard.Storage()
+    storage.create_base_storage(mgd)
+
+def main(addr):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+
+    socket.bind(addr)
+
+    while True:
+        msg = socket.recv()
+        print("%s" % (msg))
+        socket.send('hey!')
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("usage: daemon.py <address>")
+        raise SystemExit
+
+    init()
+    main(sys.argv[1])
