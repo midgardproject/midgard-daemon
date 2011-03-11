032e17d908810742d6a85a12ac6339d91d89a3e7
Alexey Zakhlestin <indeyets@gmail.com>
2011-03-11 15:22:42 +0000

    store RDF-aliases to Dictionaries instead of printing them

--- RdfMapper.py
+++ RdfMapper.py
@@ -4,7 +4,13 @@ from gi.repository import Midgard
 class RdfMapper:
     def __init__(self, mgd):
         self.mgd = mgd
+
+        self.classes = {}
+        self.fields = {}
+
         self.read_types()
+        print(self.classes)
+        print(self.fields)
 
     def read_types(self):
         for gtype in Midgard.Object.__gtype__.children:
@@ -19,16 +25,19 @@ class RdfMapper:
         mrp = Midgard.ReflectorProperty(dbclass = gtype.name)
 
         if rdf:
-            print("%s = %s" % (gtype.name, rdf))
+            self.classes[rdf] = gtype.name
+            #print("%s = %s" % (gtype.name, rdf))
         else:
-            print("%s" % (gtype.name))
+            #print("%s" % (gtype.name))
+            pass
 
         namespaces = {}
         if namespaces_str:
             for short, long in [namespace_str.split(":", 1) for namespace_str in namespaces_str.split(",")]:
                 namespaces[short] = long
-            print(namespaces)
-            
+            #print(namespaces)
+
+        properties = {}
         for property in gobject.list_properties(gtype):
             rdf_prop = mrp.get_user_value(property.name, 'property')
 
@@ -40,8 +49,11 @@ class RdfMapper:
                     rdf_prop = rdf_prop.replace(k + ":", v, 1)
                     break
 
-            print("    %s = %s" % (property.name, rdf_prop))
-        
+            properties[rdf_prop] = property.name
+            #print("    %s = %s" % (property.name, rdf_prop))
+
+        if len(properties):
+            self.fields[gtype.name] = properties
+
         for kid in gtype.children:
             self.read_type(kid)
-
